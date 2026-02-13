ae6f6cc111c5d9fa4a7dcd5218be2b4829672712
hanif <hanifsholihin884@gmail.com>
2026-02-13 11:26:19 +0700

    fix: binding 0.0.0.0 and missing ignore env

--- main.go
+++ main.go
@@ -14,10 +14,7 @@ import (
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("error load .env", err)
-	}
+	godotenv.Load()
 
 	db := database.InitDB()
 	db.AutoMigrate(&model.User{}, &model.Wallet{}, &model.Transaction{})
@@ -45,5 +42,5 @@ func main() {
 	}
 
 	log.Println("Server running on :5000")
-	r.Run(":5000")
+	r.Run("0.0.0.0:5000")
 }
